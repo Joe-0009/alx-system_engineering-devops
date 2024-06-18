de54498453d7a97eaecf68dafbac105184aa7074
Joe-0009 <youssefrachidi1994@gmail.com>
2024-06-18 12:56:08 +0100

    first and second task

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function to query hot posts on a given Reddit subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {
+        "User-Agent" : "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit" : 10
+    }
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print("None")
