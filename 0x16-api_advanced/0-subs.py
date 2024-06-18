8037d72bb793cfe989eb8d162c91afe3add700c7
Joe-0009 <youssefrachidi1994@gmail.com>
2024-06-18 15:45:11 -0700

    gg

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,11 +12,11 @@ def number_of_subscribers(subreddit):
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Mozilla/5.0"
     }
     response = requests.get(url, headers=headers,
                             allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         return 0
-    result = response.json().get('data')
-    return result.get("subscribers")
+    data = response.json()
+    return data.get("data", {}).get("subscribers", 0)
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,11 +7,13 @@ import requests
 
 def count_words(subreddit, word_list, after='', word_counts={}):
     """
-    Recursive function that queries the Reddit API, parses the title of all hot articles,
+    Recursive function that queries the Reddit API,
+    parses the title of all hot articles,
     and prints a sorted count of given keywords.
     """
     headers = {'User-Agent': 'alx-system_engineering-devops'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}'
+    url = 'https://www.reddit.com/ r/{}/hot.json?limit=100&after={}'
+    .format(subreddit, after)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if after is None:
@@ -30,7 +32,9 @@ def count_words(subreddit, word_list, after='', word_counts={}):
                 # Count the occurrences of each word in the title
                 occurrences = title.split().count(word.lower())
                 if occurrences > 0:
-                    word_counts[word.lower()] = word_counts.get(word.lower(), 0) + occurrences
+                    word_counts[word.lower()]
+                    = word_counts.get(word.lower(), 0)
+                    + occurrences
 
         # Recursive call with the next page of articles
         count_words(subreddit, word_list, data['data']['after'], word_counts)
@@ -45,6 +49,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+        print("Ex: {} programming 'python java
+              javascript'".format(sys.argv[0]))
     else:
         count_words(sys.argv[1], sys.argv[2].split())
